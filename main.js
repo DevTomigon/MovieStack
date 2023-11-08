6ed1c7c7cb40507f4447c2c2fc3c654b89939fb9
DevTomiGon <gonzaleztomas4@icloud.com>
2023-11-08 09:53:47 -0300

    version final

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ movies.forEach(movie => {
     
     
     card.innerHTML = `
-        <h2 class="text-orange-700 bg-black text-center mb-2 rounded-sm">${movie.title} </h2>
+        <h2 class="text-orange-700 bg-black text-center mb-2 rounded-sm ">${movie.title} </h2>
         <img src="${movie.image}" alt="${movie.nombre} class="rounded-lg">
         <p class="p-6">${movie.tagline}</p>
-        <p class="bg-black text-white p-6 rounded-lg">${movie.overview}</p>
+        <p class="bg-black text-white p-6 rounded-lg h-[200px] overflow-y-scroll" style="overflow-y: scroll;">
+  ${movie.overview}
+</p>
     `;
 
     const container = document.getElementById("movies-container");
