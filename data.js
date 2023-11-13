e1cf626c704a6e6c7de497ff06e233b553427fd9
DevTomiGon <gonzaleztomas4@icloud.com>
2023-11-13 17:48:50 -0300

    vfinal-moviestack

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ const movies = [
       "genres": [
         "Horror",
         "Mystery",
-        "Thriller"
+        "Thriller",
       ],
       "original_language": "en",
       "overview": "In 1956 France, a priest is violently murdered, and Sister Irene begins to investigate. She once again comes face-to-face with a powerful evil.",
